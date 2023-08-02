db6b411c2cd19591cf42a539be8745815cba8ee2
moFuhidy <mo7ammed9290@gmail.com>
2023-08-02 12:28:27 +0300

    Added the navbar for mobiile

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,20 +1,27 @@
+import eclipse from '../assets/Ellipse 1.svg';
+import eclipsBig from '../assets/Ellipsebig.svg';
+
 function Hello() {
   return (
-    <div className=" mt-24 flex flex-col gap-10">
-      <h1 className=" text-7xl font-semibold text-primary">
-        <p className=" text-sm text-text">Hello, I’m</p>
-        Mohammed
-        <br />
-        AlFuhidy
-      </h1>
+    <main className="relative">
+      <div className=" mt-24 flex flex-col gap-10 sm:relative md:relative">
+        <h1 className=" text-5xl sm:text-8xl font-semibold text-primary">
+          <p className=" text-sm sm:text-base text-text font-normal">Hello, I’m</p>
+          Mohammed
+          <br />
+          AlFuhidy
+        </h1>
 
-      <p className=" text-lg  paragraph">
-        I am a frontend developer!
-        I can help you build a product, feature or website.
-        Look through some of my work and experience!
-        If you like what you see and have a project you need coded, don’t hesitate and contact me.
-      </p>
-    </div>
+        <p className=" text-xs sm:text-lg  paragraph">
+          I am a frontend developer!
+          I can help you build a product, feature or website.
+          Look through some of my work and experience!
+          If you like what you see and have a project you need coded, don’t hesitate and contact me.
+        </p>
+      </div>
+      <img src={eclipse} alt="circle" className="h-14 fixed top-32 right-8 sm:hidden" />
+      <img src={eclipsBig} alt="circle" className="sm:fixed top-32 sm:right-10 md:right-96 hidden sm:block" />
+    </main>
   );
 }
 export default Hello;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,40 @@
+import { useState } from 'react';
+import { CgMenuRightAlt, CgClose } from 'react-icons/cg/index';
+import eclipse from '../assets/Ellipse 1.svg';
+
 function Navbar() {
+  const [nav, setNav] = useState(false);
+
+  const showNav = () => {
+    setNav(!nav);
+  };
   return (
     <nav className=" h-20 flex justify-between items-center ">
       <h1 className=" text-xl">Fuhidy</h1>
-      <ul className="flex gap-4 items-center">
-        <li><a href="#home">Hello</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#service">Services</a></li>
-        <li className=" bg-primary text-background py-2 px-4 rounded-3xl"><a href="#contact">Contact</a></li>
+      <ul className=" gap-4 items-center hidden sm:flex">
+        <li><a className="" href="#home">Hello</a></li>
+        <li><a className="" href="#portfolio">Portfolio</a></li>
+        <li><a className="" href="#service">Services</a></li>
+        <li className=" bg-primary text-background py-2 px-4 rounded-3xl"><a className="text-base sm-text-lg" href="#contact">Contact</a></li>
+      </ul>
+
+      {/* hamburger */}
+      {nav ? (
+        <CgClose className="w-8 h-8 z-50 right-[15px] fixed text-text sm:hidden" onClick={showNav} />
+      ) : (
+        <CgMenuRightAlt className="w-8 h-8 text-text sm:hidden" onClick={showNav} />
+      )}
+
+      {/* second nav */}
+      <ul className={`h-[100vh] pt-32 gap-12 fixed top-[0px] flex flex-col w-full sm:hidden bg-background z-40 duration-1000 ${
+        nav ? 'right-[0px]' : 'right-[-100vw]'
+      } `}
+      >
+        <li><a className="p-20 sm-text-lg text-text text-4xl" href="#home">Hello</a></li>
+        <li><a className="p-20 sm-text-lg text-text text-4xl" href="#portfolio">Portfolio</a></li>
+        <li><a className="p-20 sm-text-lg text-text text-4xl" href="#service">Services</a></li>
+        <li><a className="p-20 sm-text-lg text-text text-4xl" href="#contact">Contact</a></li>
+        <img src={eclipse} alt="circle" className="h-10 pr-60" />
       </ul>
     </nav>
   );
