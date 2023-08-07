dd74c5fb590fd29fad4ef2f81a2caabfca710194
Mohammed Al-Fuhidy <mo7ammed9290@gmail.com>
2023-08-07 19:07:06 +0300

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,7 @@ function Projects() {
       <h2 className="myName sm:text-[6.8rem] text-5xl text-background projectsTitle absolute sm:pt-5 pt-5 ">
         Projects
       </h2>
-      <div className=" w-full sm:py-44 py-32 ">
+      <div className=" w-full sm:py-44 pt-32 pb-10 ">
         <MansoryLayout />
       </div>
     </div>
