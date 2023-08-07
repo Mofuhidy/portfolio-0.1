414b8f5e1763c4621b9a75da8405540d9d05d0d7
moFuhidy <mo7ammed9290@gmail.com>
2023-08-07 18:46:43 +0300

    Fix linter error

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,7 @@ import MansoryLayout from './MansoryLayout';
 
 function Projects() {
   return (
-    <div className="mt-40 bg-primary min-h-auto md:px-20 py-0 px-4 relative" id="portfolio">
+    <div className="mt-40 bg-primary min-h-full md:px-20 py-0 px-4 relative" id="portfolio">
       <h2 className="myName sm:text-[6.8rem] text-5xl text-background projectsTitle absolute sm:pt-5 pt-5 ">
         Projects
       </h2>
